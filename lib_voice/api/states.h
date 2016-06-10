6a0a0f5fc83371e603d22322737306ec7a5c20f1
Sam Chesney <samc@xmos.com>
2016-06-10 12:11:09 +0100

    Add Alexa model data and make keyword a compile time option

--- lib_voice/api/states.h
+++ lib_voice/api/states.h
@@ -1,7 +1,29 @@
 #ifndef STATES_H
 #define STATES_H
 
+#ifndef KEYWORD_SUZY
+#define KEYWORD_SUZY 0
+#endif
+
+#ifndef KEYWORD_ALEXA
+#define KEYWORD_ALEXA 1
+#endif
+
+#if KEYWORD_SUZY && KEYWORD_ALEXA
+#error "Cannot enable multiple keywords"
+#endif
+
+#if !KEYWORD_SUZY && !KEYWORD_ALEXA
+#error "No keyword enabled"
+#endif
+
+#if KEYWORD_SUZY
+#define STATES 4
+#elif KEYWORD_ALEXA
 #define STATES 6
+#else
+#error "Cannot define correct number of STATES"
+#endif
 #define FEATURES 23
 #define MAXSTEPS 92
 
@@ -19,10 +41,29 @@ typedef struct {
     state states[STATES];
 } hmm;
 
+#ifndef SPEAKER_HM
+#define SPEAKER_HM 1
+#endif
+
+#ifndef SPEAKER_ML
+#define SPEAKER_ML 1
+#endif
+
+#ifndef SPEAKER_RO
+#define SPEAKER_RO 1
+#endif
+
+#ifndef SPEAKER_SC
+#define SPEAKER_SC 1
+#endif
+
 extern hmm suzy_mark;
 extern hmm suzy_al_ro;
 extern hmm suzy_post_illusonic_ro;
 
+extern hmm alexa_hm;
+extern hmm alexa_ml;
 extern hmm alexa_ro;
+extern hmm alexa_sc;
 
 #endif
